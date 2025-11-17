72f4689b694c8aef574bb5e973eaecb0f4f7549d
Rudra Hatte <rudrarh1806@gmail.com>
2025-11-17 05:53:22 +0530

    Working React build - ready for Render deployment

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ARScanner from './components/ARScanner';
+
+function App() {
+  return (
+    <div className="app">
+      <ARScanner />
+    </div>
+  );
+}
+
+export default App;
